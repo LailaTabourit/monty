a273ab1966b7549807db3095e2139a05966c04d1
l11EG4 <zakariaberahmi@gmail.com>
2023-06-20 13:09:41 -0700

    Mega

--- monty.h
+++ monty.h
@@ -43,7 +43,7 @@ typedef struct instruction_s
 /*----------------------ProtoTypes----------------------*/
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
-
+void pint(stack_t **head, unsigned int counter);
 
 
 #endif /* MONTLY_H */
